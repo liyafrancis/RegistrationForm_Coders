17638b20ecaab92bfa0b2de3ce162ef3726f753a
2432774 <2432774@DESKTOP-S160R83>
2025-08-14 09:26:48 +0530

    Add project files.

---
+++ RegistrationForm/Controllers/RegistrationController.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using RegistrationForm.Models;
+using System;
+using System.Web.Mvc;
+
+namespace RegistrationForm.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private string connectionString = "server=localhost;user id=root;password=root;database=registration_db";
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "INSERT INTO users (first_name, last_name, dob, place) VALUES (@first, @last, @dob, @place)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@first", user.First_Name);
+                    cmd.Parameters.AddWithValue("@last", user.Last_Name);
+                    cmd.Parameters.AddWithValue("@dob", user.DOB);
+                    cmd.Parameters.AddWithValue("@place", user.Place);
+                    cmd.ExecuteNonQuery();
+
+                    ViewBag.Message = "Registration successful!";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "Error: " + ex.Message;
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ RegistrationForm/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationForm.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string Place { get; set; }
+    }
+}
